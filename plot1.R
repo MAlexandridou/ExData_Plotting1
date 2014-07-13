c33ec7259406493526994b8844bd9bd499bb2d52
MAlexandridou <MAlexandridou@users.noreply.github.com>
2014-07-13 17:59:18 +0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -9,6 +9,7 @@ data<-as.data.frame(cbind(Date,first$Time,first$Global_active_power,
 names(data)<-c("Date","Time","Global_active_power", "Global_reactive_power", 
                "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2",
                "Sub_metering_3")
+               ## For Some reason it calculates the day next to the date i have entered...
 data<-subset(data,as.Date(Date) >= '2007-01-31' & as.Date(Date)<='2007-02-01')
 
 png(filename="plot1.png",width=480,height=480)
